c6bf5a9deb5ab93e9df8f926cd527456a7f90ea3
AErencihan <eren.cihann@hotmail.com>
2025-06-21 22:38:23 +0300

    Add initial application configuration and Docker setup
    - Configure application properties for PostgreSQL and JPA
    - Create Dockerfile for building and running the application
    - Add docker-compose.yml for Redis and PostgreSQL services
    - Implement a test API endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM ubuntu:latest
-LABEL authors="erencihan"
+FROM maven:3.8.4-openjdk-17-slim AS build
+WORKDIR /app
+COPY pom.xml .
+COPY src ./src
+RUN mvn clean install -DskipTests
 
-ENTRYPOINT ["top", "-b"]
+FROM openjdk:17 AS runtime
+WORKDIR /app
+COPY --from=build /app/target/flightTicketApp-0.0.1-SNAPSHOT.jar .
+CMD ["java", "-jar", "flightTicketApp-0.0.1-SNAPSHOT.jar"]
--- src/main/java/org/example/flightticketapp/controller/TestApi.java
+++ src/main/java/org/example/flightticketapp/controller/TestApi.java
@@ -1,4 +1,19 @@
 package org.example.flightticketapp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class TestApi {
+
+
+    @GetMapping("/api")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test API is working!");
+    }
+
+
 }
